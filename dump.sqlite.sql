7802b993ae09cf6605fec3dec0578c95eb420771
abinjacob-dev <157444994+abinjacob-dev@users.noreply.github.com>
2024-01-28 12:40:21 +0530

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,30 @@
+-- TABLE
+CREATE TABLE customers (
+  customer_id INT NOT NULL,
+  customer_fname TEXT,
+  customer_lname TEXT,
+  PRIMARY key (customer_id)
+  );
+CREATE TABLE orders (
+  order_id INT NOT NULL,
+  order_number TEXT,
+  customer_id INT,
+  product_id INT,
+  PRIMARY key (order_id),
+  FOREIGN key (customer_id) REFERENCES customers(customer_id)
+  FOREIGN key (product_id) REFERENCES products(product_id)
+  );
+CREATE TABLE products (
+  product_id INT NOT NULL,
+  product_name TEXT,
+  product_price MONEY, 
+  product_stock INTEGER,
+  PRIMARY KEY (product_id)
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
